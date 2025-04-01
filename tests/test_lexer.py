25783b01b2e1b53a79adad46eafe39a1bf781596
voidbert <humbertogilgomes@protonmail.com>
2025-04-02 00:16:35 +0100

    Add lexer testing utilities

---
+++ tests/test_lexer.py
@@ -0,0 +1,55 @@
+# -------------------------------------------- LICENSE --------------------------------------------
+#
+# Copyright 2025 Humberto Gomes, José Lopes, José Matos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -------------------------------------------------------------------------------------------------
+
+from typing import Callable
+from plpc.lexer import LexerError, create_lexer
+
+# ------------------------------------------ EASE OF USE ------------------------------------------
+
+SimpleToken = tuple[str, str]
+
+def successful_test(source: str) -> Callable[[Callable[[], list[SimpleToken]]], Callable[[], None]]:
+    def decorator(test: Callable[[], list[SimpleToken]]) -> Callable[[], None]:
+        def wrapper() -> None:
+            lexer = create_lexer('<test-input>')
+            lexer.input(source)
+
+            got = [ (t.type, t.value) for t in lexer ]
+            expected = test()
+
+            assert got == expected
+
+        return wrapper
+
+    return decorator
+
+def failing_test(source: str) -> Callable[[Callable[[], None]], Callable[[], None]]:
+    def decorator(_: Callable[[], None]) -> Callable[[], None]:
+        def wrapper() -> None:
+            try:
+                lexer = create_lexer('<test-input>')
+                lexer.input(source)
+                _ = list(lexer)
+            except LexerError:
+                return
+
+            assert False
+
+        return wrapper
+
+    return decorator
