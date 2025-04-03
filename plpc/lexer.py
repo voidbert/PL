f9e616badf70ae3a227b115ea8db6873797d2023
Voidbert <humbertogilgomes@protonmail.com>
2025-04-03 14:50:41 +0100

    Separate error formatting to another module

--- plpc/lexer.py
+++ plpc/lexer.py
@@ -16,19 +16,35 @@
 #
 # -------------------------------------------------------------------------------------------------
 
-import sys
+import dataclasses
 import re
 import ply.lex
 
+from .error import print_error
+
 class LexerError(ValueError):
     pass
+
+@dataclasses.dataclass
+class IntegerTokenValue:
+    value: int
+    string_value: float
+
+@dataclasses.dataclass
+class FloatTokenValue:
+    value: float
+    string_value: float
+
+@dataclasses.dataclass
+class StringTokenValue:
+    value: float
+    string_value: float
 
 class _Lexer:
     def __init__(self, file_path: str):
         self.has_errors = False                        # If errors have been detected by the lexer
         self.file_path = file_path                     # Path to the source for error printing
         self.last_error: tuple[int, int] | None = None # Start position and length of error
-        self.line_start = 0                            # Start position of the current line
 
         self.literals = '.;:(,)<>=+-*/[]^'
         self.keywords = {
@@ -112,17 +128,20 @@ class _Lexer:
 
     def t_FLOAT(self, t: ply.lex.LexToken) -> ply.lex.LexToken:
         r'[0-9]+(\.[0-9]+e(\+|\-)?[0-9]+|\.[0-9]+|e(\+|\-)?[0-9]+)'
-        t.value = float(t.value)
+        converted = float(t.value)
+        t.value = FloatTokenValue(converted, t.value)
         return t
 
     def t_INTEGER(self, t: ply.lex.LexToken) -> ply.lex.LexToken:
         r'[0-9]+'
-        t.value = int(t.value)
+        converted = int(t.value)
+        t.value = IntegerTokenValue(converted, t.value)
         return t
 
     def t_STRING(self, t: ply.lex.LexToken) -> ply.lex.LexToken:
         r'\'((?:\'\'|[^\'])*)\''
-        t.value = t.value[1:-1].replace('\'\'', '\'')
+        converted = t.value[1:-1].replace('\'\'', '\'')
+        t.value = StringTokenValue(converted, t.value)
         return t
 
     def t_ALT_CARET(self, t: ply.lex.LexToken) -> ply.lex.LexToken:
@@ -145,7 +164,6 @@ class _Lexer:
 
         self.commit_error(t.lexer)
         t.lexer.lineno += len(t.value)
-        self.line_start = t.lexer.lexpos
 
     def t_eof(self, t: ply.lex.LexToken) -> ply.lex.LexToken:
         self.t_newline(t) # Commit error if need be
@@ -170,31 +188,17 @@ class _Lexer:
 
     def commit_error(self, lexer: ply.lex.Lexer) -> None:
         if self.last_error is not None:
-            # Determine important positions in the text
-            line_end = lexer.lexdata.find('\n', self.line_start)
-            if line_end == -1:
-                line_end = lexer.lexlen
-
             start, length = self.last_error
-            relative_start = start - self.line_start
-            length = min(length, line_end - start)
 
-            # Pretty print error
-            line = lexer.lexdata[self.line_start:line_end]
-
-            error_location = f'{self.file_path}:{lexer.lineno}:{relative_start + 1}'
-            error_description = 'Lexer failed to reconize the following characters here'
-            error_underline = ' ' * relative_start + '\033[91m^' + '~' * (length - 1) + '\033[0m'
-
-            print(f'{error_location}: \033[91merror\033[0m: {error_description}', file=sys.stderr)
-            print(f'{lexer.lineno: 6d} | {line}', file=sys.stderr)
-            print(f'         {error_underline}\n', file=sys.stderr)
+            print_error(self.file_path,
+                        lexer.lexdata,
+                        'Lexer failed to reconize the following characters',
+                        lexer.lineno,
+                        start,
+                        length)
 
             self.has_errors = True
             self.last_error = None
 
 def create_lexer(file_path: str) -> ply.lex.Lexer:
     return ply.lex.lex(module=_Lexer(file_path), reflags=re.IGNORECASE)
-
-def get_tokens() -> list[str]:
-    return _Lexer('').tokens
